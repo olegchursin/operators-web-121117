161d6a1d850ea7cf34f8678cfe0c6472e4130a5e
Oleg Chursin <olegchursin@gmail.com>
2017-11-26 08:36:57 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,7 @@
 def unsafe?(speed)
-  if speed > 60 and speed < 40
+  if speed > 60
+		true
+	elsif speed < 40
 		true
 	else
 		false
@@ -9,5 +11,6 @@ end
 
 
 def not_safe?(speed)
-	speed > 60 && speed < 40 ? true : false
+	speed > 60 ? true : false
+	speed < 40 ? true : false
 end
